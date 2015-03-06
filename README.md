e6700e5c2944ea9c0ed59f3e003b10555da532b1
Sandeep Dolia <UC136988@UC136988-TPD-A.ten.thomsonreuters.com>
2015-03-06 15:47:15 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+Spring Hibernate application
+===========
+
+
+MySQL -> Hibernate -> Spring MVC -> JSP
+
+
+1. Eclipse Kepler
+   1.1 Spring Tool Suite (STS) 3.4.0.RELEASE - for Keepler
+2. Java SDK 1.7.0_51 (separate install)
+3. Tomcat 7.0.50 (separate install)
+4. Maven 3.0.5 (separate install)
+5. MySQL 5.5.29
---
+++ src/main/java/com/sanri/app/dao/StrategyDAO.java
@@ -0,0 +1,15 @@
+package com.sanri.app.dao;
+
+import java.util.List;
+
+import com.sanri.app.model.Strategy;
+
+public interface StrategyDAO {
+
+    public void addStrategy(Strategy strategy);
+    public Strategy getStrategy(int id);
+    public void updateStrategy(Strategy strategy);
+    public void deleteStrategy(int id);
+    public List<Strategy> getStrategies();
+    
+}
